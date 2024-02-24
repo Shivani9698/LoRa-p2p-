569edfb2e031ecbc8006f3220dc5a4856009aac7
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-02-24 17:16:17 +0530

    Update DC Motor.ino

--- DC Motor.ino
+++ DC Motor.ino
@@ -1,3 +1,10 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 14
+#define dio0 2
 // Motor A
 int motor1Pin1 = 13; 
 int motor1Pin2 = 26; 
@@ -9,7 +16,10 @@ const int pwmChannel = 0;
 const int resolution = 8;
 int dutyCycle = 200;
 
+int counter = 0;
+
 void setup() {
+
   // sets the pins as outputs:
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
@@ -21,33 +31,63 @@ void setup() {
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(enable1Pin, pwmChannel);
 
-  Serial.begin(115200);
-
   // testing
+ // Serial.print("Testing DC Motor...");
+  //initialize Serial Monitor
+  Serial.begin(115200);
   Serial.print("Testing DC Motor...");
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(866E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+  
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
   // Move the DC motor forward at maximum speed
   Serial.println("Moving Forward");
+  LoRa.println("Moving Forward");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, HIGH); 
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
+  LoRa.println("Motor stopped");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
   delay(1000);
 
   // Move DC motor backwards at maximum speed
   Serial.println("Moving Backwards");
+  LoRa.println("Moving Backwards");
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW); 
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
+  LoRa.println("Motor stopped");
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
   delay(1000);
@@ -59,8 +99,16 @@ void loop() {
     ledcWrite(pwmChannel, dutyCycle);   
     Serial.print("Forward with duty cycle: ");
     Serial.println(dutyCycle);
+    LoRa.print("Forward with duty cycle: ");
+    LoRa.println(dutyCycle);
     dutyCycle = dutyCycle + 5;
     delay(500);
   }
   dutyCycle = 200;
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(10000);
 }
